acb17e49c0d5e5c0cfc7d2a30fd05a000b228d29
KattyaCuevas <kattya.cuevas@gmail.com>
2017-04-11 14:03:08 -0500

    Token authentication, by header

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def restrict_access
-    api_key = ApiKey.find_by_access_token(params[:access_token])
-    head :unauthorized unless api_key
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
   end
 end
